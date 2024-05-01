0cc1608ac1687bb7cce45abf73d6ad178e3dedbf
PohangCandy <rlcks777@naver.com>
2024-05-01 18:59:46 +0900

    learning obj array

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,49 +1,59 @@
+#include <Windows.h>
+#include <conio.h>
 #include <iostream>
 
 using namespace std;
 
-class PizzaStore {
-	int pizza_size;
-
+class Circle {
 public:
-
-	PizzaStore(int psize) :pizza_size{ psize }
-	{
-	}
-
-	int getPizzaSize()
-	{
-		return pizza_size;
-	}
-
-	void setPizzaSize(int newSize)
-	{
-		pizza_size = newSize;
-	}
-	void print();
+	Circle() : x(0), y(0), radius(0) {};
+	Circle(int _x, int _y, int _radius) : x(_x), y(_y), radius(_radius) {};
+	void draw();
+private:
+	int x, y;
+	int radius;
+public:
+	int GetXpos() { return x; }
+	int GetYpos() { return y; }
+	int GetRadius() { return radius; }
+	void SetXpos(int newXpos) { x = newXpos; }
+	void SetYpos(int newYpos) { y = newYpos; }
+	void SetRadius(int newRadius) { radius = newRadius; }
 };
 
-void sizeUP(PizzaStore &P)
-{
-	P.setPizzaSize(P.getPizzaSize() * 2);
+int main() {
+
+	Circle circles[10];
+	while (true)
+	{
+		for (Circle& c : circles)
+		{
+			c.SetXpos(rand() % 500);
+			c.SetYpos(rand() % 300);
+			c.SetRadius(rand() % 100);
+			c.draw();
+		}
+		char ch = _getch();
+		if (ch == 'q')
+		{
+			break;
+		}
+		if (ch == 'm')
+		{
+			for (Circle& c : circles)
+			{
+				c.SetXpos(c.GetXpos() + 1000);
+				c.draw();
+			}
+		}
+	}
+
+
 }
 
-void PizzaStore::print()
+void Circle::draw()
 {
-	cout << getPizzaSize();
-}
-
-PizzaStore returnPizzaObj()
-{
-	PizzaStore p{20};
-		return p;
-}
-
-int main()
-{
-	PizzaStore P{ 10 };
-	P = returnPizzaObj();
-	P.print();
-
-	return 0;
+	int r = radius / 2;
+	HDC hdc = GetWindowDC(GetForegroundWindow());
+	Ellipse(hdc, x - r, y - r, x + r, y + r);
 }
