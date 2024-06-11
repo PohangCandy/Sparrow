20934aca2291fa984472e65c84271db5d42bcd85
PohangCandy <rlcks777@naver.com>
2024-06-11 22:05:12 +0900

    making lunar game

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,21 +1,47 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // 그림이 그려지는 화면 생성
+    RenderWindow window(VideoMode(600, 480), "LUNAR LANDER");
+    window.setFramerateLimit(60);
+
+    // 게임에 필요한 스프라이트 생성
+    Texture t1;
+    Sprite Background;
+    t1.loadFromFile("images/background.png");
+    Background.setTexture(t1);
+
+    Texture t2; // 텍스트 객체
+    Sprite lander; // 스프라이트 객체
+    t2.loadFromFile("images/spaceship.png"); // 이미지 파일을 텍스처 객체로 적재한다.
+    lander.setTexture(t2); // 스프라이트의 텍스처를 설정한다.
+
+
+    // 여기서부터 게임 루프
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        // 이벤트 검사 및 처리
+        Event e;
+        while (window.pollEvent(e))
         {
-            if (event.type == sf::Event::Closed)
+            if (e.type == Event::Closed)
                 window.close();
         }
 
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+            lander.move(-10.0, 0.0);
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+            lander.move(10.0, 0.0);
+
+
+        // 화면을 지운다
         window.clear();
-        window.draw(shape);
+        // 화면에 스프라이트를 그린다
+        window.draw(Background);
+        window.draw(lander);
+        // 화면을 표시한다
         window.display();
     }
 
