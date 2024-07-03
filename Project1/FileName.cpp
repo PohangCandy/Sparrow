229dac8ad9a78a0b596921619415443007c3a032
PohangCandy <rlcks777@naver.com>
2024-07-03 18:07:50 +0900

    learning about deep copy

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,82 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class Time
-{
+class MyArray {
 public:
-	int hour, min, sec;
-	Time() {}
-	Time(int h, int m, int s) : hour(h), min(m), sec(s) {};
-	~Time() {};
+	int size;
+	int* data;
+	MyArray(const MyArray& other);
+	MyArray(int size);
 
-	void print() { cout << hour << ":" << min << ":" << sec << endl; }
-	int getHour() { return this->hour; }
-	int getMin() { return this->hour; }
-	int getSec() { return this->hour; }
-	void setHour(int newHour) { this->hour = newHour; }
-	void setMin(int newMin) { this->min = newMin; }
-	void setSec(int newSec) { this->sec = newSec; }
+	~MyArray()
+	{
+		if (data != NULL) delete[] this->data;
+	}
 };
 
-bool isEqual(Time& t1, Time& t2)
-{
-	t1.print();
-	t2.print();
-	if (t1.hour == t2.hour)
-	{
-		if (t1.min == t2.min)
-		{
-			if (t1.sec == t2.sec)
-			{
-				return true;
-			}
-		}
-	}
-	return false;
-}
-bool isEqual(Time* pt1, Time* pt2)
-{
-	pt1->print();
-	pt2->print();
-	if (pt1->hour == pt2->hour)
-	{
-		if (pt1->min == pt2->min)
-		{
-			if (pt1->sec == pt2->sec)
-			{
-				return true;
-			}
-		}
-	}
-	return false;
-}
+
 
 int main()
 {
-	Time* pTime = new Time[2];
-	for (int i = 0; i < 2; i++)
+	MyArray buffer(10);
+	buffer.data[0] = 1; 
 	{
-		cout << i << "번째 시간 객체의 시간을 입력하시오: ";
-		int newhour;
-		cin >> newhour;
-		pTime[i].setHour(newhour);
-
-		cout << i << "번째 시간 객체의 분을 입력하시오: ";
-		int newmin;
-		cin >> newmin;
-		pTime[i].setMin(newmin);
-
-		cout << i << "번째 시간 객체의 초를 입력하시오: ";
-		int newsec;
-		cin >> newsec;
-		pTime[i].setSec(newsec);
-
+		MyArray clone = buffer;
 	}
-	
-	if (isEqual(pTime[0], pTime[1]))
-	{
-		cout << "시간이 일치함" << endl;
-	}
+	buffer.data[0] = 2;
 
 	return 0;
+}
+
+MyArray::MyArray(const MyArray& other)
+{
+	this->size = other.size;
+	this->data = new int[other.size]; //새로운 공간 할당
+	for (int i = 0; i < other.size; i++) // 데이터 복사
+	{
+		this->data[i] = other.data[i];
+	}
+}
+
+MyArray::MyArray(int size)
+{
+	this->size = size;
+	data = new int[size];
 }
