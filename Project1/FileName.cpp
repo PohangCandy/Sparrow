9a7135563065695cb2529e8e5fad4f7fa6264958
PohangCandy <rlcks777@naver.com>
2024-07-03 18:27:13 +0900

    learning about copy constructor

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,45 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class MyArray {
+class Person {
+	
 public:
-	int size;
-	int* data;
-	MyArray(const MyArray& other);
-	MyArray(int size);
-
-	~MyArray()
-	{
-		if (data != NULL) delete[] this->data;
-	}
+	int age;
+	Person(int a):age(a){}
 };
-
 
 
 int main()
 {
-	MyArray buffer(10);
-	buffer.data[0] = 1; 
+	Person p1(10);
+	Person p2(10);
+
+	if (p1 == p2)
 	{
-		MyArray clone = buffer;
+		cout<< "같습니다." << endl;
 	}
-	buffer.data[0] = 2;
+
 
 	return 0;
-}
-
-MyArray::MyArray(const MyArray& other)
-{
-	this->size = other.size;
-	this->data = new int[other.size]; //새로운 공간 할당
-	for (int i = 0; i < other.size; i++) // 데이터 복사
-	{
-		this->data[i] = other.data[i];
-	}
-}
-
-MyArray::MyArray(int size)
-{
-	this->size = size;
-	data = new int[size];
 }
