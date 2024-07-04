97724d5fa1f1425f0119b034df035a5e4268a16d
PohangCandy <rlcks777@naver.com>
2024-07-04 17:38:21 +0900

    learning about singleton pattern

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Person {
-	
+class MyClass {
 public:
-	int age;
-	Person(int a):age(a){}
-};
+	int x;
+	char c;
+	string s;
+	MyClass(int _x, char _c, string _s) : x(_x), c(_c), s(_s) { cout<<"积己等 扁夯 积己磊"<<endl; }
+	MyClass(const MyClass& other) : x(other.x), c(other.c), s(other.s) { cout << "积己等 汗荤 积己磊" << endl; }
+	~MyClass() {}
 
+};
 
 int main()
 {
-	Person p1(10);
-	Person p2(10);
-
-	if (p1 == p2)
-	{
-		cout<< "鞍嚼聪促." << endl;
-	}
-
-
+	MyClass c1(5, '3', "Jhon");
+	MyClass c2 = c1;
 	return 0;
 }
