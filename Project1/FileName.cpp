3f81ac0f995dd993f70f0ca1e372d6f5029fae39
PohangCandy <rlcks777@naver.com>
2024-07-04 18:29:08 +0900

    solving exercise problem

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class MyClass {
+class Game {
 public:
-	int x;
-	char c;
-	string s;
-	MyClass(int _x, char _c, string _s) : x(_x), c(_c), s(_s) { cout<<"积己等 扁夯 积己磊"<<endl; }
-	MyClass(const MyClass& other) : x(other.x), c(other.c), s(other.s) { cout << "积己等 汗荤 积己磊" << endl; }
-	~MyClass() {}
+	static int playercount;
 
+	Game() { playercount++; }
+
+	void print() { cout << playercount << endl; }
 };
+
+int Game::playercount = 0;
+
 
 int main()
 {
-	MyClass c1(5, '3', "Jhon");
-	MyClass c2 = c1;
+	Game player1;
+	Game player2;
+	player2.print();
+
+
 	return 0;
 }
