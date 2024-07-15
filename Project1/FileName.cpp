b4b9dc5067d938acc5abfcec364270fefaf69472
PohangCandy <rlcks777@naver.com>
2024-07-15 18:51:46 +0900

    learning about inheritance

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class Box {
-private:
-	double length;
-	double width;
-	double height;
-public:
-	Box(int l = 0,int w = 0,int h = 0) : length(l),width(w),height(h){}
-	double getVolume(void)
-	{
-		return length * width * height;
-	}
+class Car {
+	int Speed = 10;
+};
 
-	friend void printBox(Box b);
-
+class SpeedCar : public Car
+{
+	bool turbo;
 };
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	Box b1(10, 20, 30);
-	printBox(b1);
+
 
 
 	return 0;
 
 }
 
-void printBox(Box b)
-{
-	cout << b.length;
-	cout << b.width;
-	cout << b.height;
-}
+
