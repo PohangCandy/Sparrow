665f3f5d5ff6c9a97f9f224c6cad0152e95aa5d0
PohangCandy <rlcks777@naver.com>
2024-07-16 18:50:55 +0900

    learning about overriding

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Car {
-	int Speed = 10;
+class Animal {
+public:
+	void speak()
+	{
+		cout << "悼拱捞 家府甫 郴绊 乐澜" << endl;
+	}
 };
 
-class SpeedCar : public Car
-{
-	bool turbo;
+class Dog : public Animal {
+public:
+	void speak()
+	{
+		cout << "港港!" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void speak()
+	{
+		cout << "具克!" << endl;
+	}
 };
 
 int main()
@@ -15,10 +30,11 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	Dog obj1;
+	obj1.speak();
 
+	Cat obj2;
+	obj2.speak();
 
 	return 0;
-
 }
-
-
