b8a441a31e967c24148dddb5fb537e61e1019a20
PohangCandy <rlcks777@naver.com>
2024-08-21 20:42:51 +0900

    class template

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,33 +1,23 @@
+
 #include <iostream>
+#include <cstring>  // For std::strcpy_s and std::strlen
+
 using namespace std;
 
-int main()
+template<typename T1 , typename T2>
+void copy(T1 a1[], T2 a2[], int n)
 {
-	int pizza_slices = 12;
-	int persons = 0;
-	int slices_pre_person = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        a1[i] = a2[i];
+    }
+}
 
-	try {
-		cout << "피자 조각수를 입력하시오: ";
-		cin >> pizza_slices;
-		cout << "사람 수를 입력하시오: ";
-		cin >> persons;
 
-		if (persons == 0)
-			throw persons;
-		if (persons < 0)
-			throw "negative";
-		slices_pre_person = pizza_slices / persons;
-		cout << "한 사람당 피자는 " << slices_pre_person << "입니다." << endl;
-	}
-	catch (const char* e)
-	{
-		cout << "오류 : 사람수가 " << e << "입니다. ";
-	}
-	catch (int e)
-	{
-		cout << "오류 : 사람수가 " << e << "입니다. ";
-	}
+int main() {
+    int a[100];
+    double b[100];
+    copy(a, b, 100);
 
-	return 0;
+    return 0;
 }
