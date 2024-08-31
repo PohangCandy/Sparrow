89d6907db6b84a49d642f25d7ab843eecdd724e5
PohangCandy <rlcks777@naver.com>
2024-08-31 22:17:34 +0900

    learning dequeue

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 
-	//컨테이너에서 3의 배수를 골라서 삭제하는 코드
+	//3의 배수를 컨테이너에서 제거하는 프로그램
 	vector<int> vec;
 
 	for (int i = 0; i < 20; i++)
@@ -14,8 +14,8 @@ int main()
 		vec.push_back(i);
 	}
 
-	auto it = vec.begin();
-
+	vector<int>::iterator it = vec.begin();
+	//auto it = vec.begin();
 	while (it != vec.end())
 	{
 		if (*it % 3 == 0)
@@ -23,7 +23,15 @@ int main()
 			it = vec.erase(it);
 		}
 		else
+		{
 			it++;
+		}
+	}
+
+
+	for (auto& a : vec)
+	{
+		cout << a << " ";
 	}
 
 	return 0;
