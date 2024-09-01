fb48a37f5806a37b8d2f830507b6f6990ce02b0d
PohangCandy <rlcks777@naver.com>
2024-09-01 18:27:07 +0900

    learning deque

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,38 +1,18 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 
-	//3의 배수를 컨테이너에서 제거하는 프로그램
-	vector<int> vec;
+	deque<int> dq = { 1,2,3,4,5,6,7,8,9,10 };
 
-	for (int i = 0; i < 20; i++)
-	{
-		vec.push_back(i);
-	}
-
-	vector<int>::iterator it = vec.begin();
-	//auto it = vec.begin();
-	while (it != vec.end())
-	{
-		if (*it % 3 == 0)
-		{
-			it = vec.erase(it);
-		}
-		else
-		{
-			it++;
-		}
-	}
-
-
-	for (auto& a : vec)
-	{
-		cout << a << " ";
-	}
+	dq.pop_front();
+	dq.push_back(11);
+	for (auto& n : dq)
+		cout << n << " ";
+	cout << endl;
 
 	return 0;
 }
