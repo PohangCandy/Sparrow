571fa6dba7d88e3c2b6cf3173647114f606a1935
PohangCandy <rlcks777@naver.com>
2024-09-02 20:35:39 +0900

    try count_if fuc prob

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,34 +1,29 @@
+//짝수를 찾는 프로그램을 만들어본다.
+//count_if()를 사용한다.
 #include <iostream>
-#include <map>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
+template <typename T>
+bool is_even(const T& num)
+{
+	return num % 2 == 0;
+}
+
+using namespace std;
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	//입력한 문장으로부터 단어의 빈도를 계산하는 프로젝트
-	map<string, int> word_num;
-	string s;
-	//eof()가 나오기 전까지 단어를 입력받는다.
-	while (1)
+	vector<int> vec;
+
+	for (int i = 0; i < 10; i++)
 	{
-		//string s;
-		cin >> s;
-		if (cin.eof())
-		{
-			break;
-		}
-		word_num[s]++;
+		vec.push_back(i);
 	}
 
-	for (auto it = word_num.begin(); it != word_num.end(); it++)
-	{
-		cout << it->first << " " << it->second;
-		cout << "\n";
-	}
+	int n = count_if(vec.begin(), vec.end(), is_even<int>);
 
-	//string input;
-	//cin >> input;
-	//cout << input;
+	cout << "값이 짝수인 요소의 개수: " << n << "\n";
 
 	return 0;
 }
