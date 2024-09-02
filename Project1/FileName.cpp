124801f52be993860b6aae1d3fc2df8d87222198
PohangCandy <rlcks777@naver.com>
2024-09-02 16:21:17 +0900

    leaning map

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
-#include <deque>
+#include <map>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
+	//입력한 문장으로부터 단어의 빈도를 계산하는 프로젝트
+	map<string, int> word_num;
+	string s;
+	//eof()가 나오기 전까지 단어를 입력받는다.
+	while (1)
+	{
+		//string s;
+		cin >> s;
+		if (cin.eof())
+		{
+			break;
+		}
+		word_num[s]++;
+	}
 
-	deque<int> dq = { 1,2,3,4,5,6,7,8,9,10 };
+	for (auto it = word_num.begin(); it != word_num.end(); it++)
+	{
+		cout << it->first << " " << it->second;
+		cout << "\n";
+	}
 
-	dq.pop_front();
-	dq.push_back(11);
-	for (auto& n : dq)
-		cout << n << " ";
-	cout << endl;
+	//string input;
+	//cin >> input;
+	//cout << input;
 
 	return 0;
 }
